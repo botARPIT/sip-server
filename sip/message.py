8d80df1a51292a56ea5985330ddc82f569cabdea
Arpit Chauhan <arpit@Arpits-MacBook-Air.local>
2026-08-09 22:08:38 +0530

    refactor: raw SIP parse logic

--- sip/message.py
+++ sip/message.py
@@ -3,25 +3,30 @@
 
 class SIPMessage:
 
-    def __init__(self, header, body):
+    def __init__(self, start_line, header, body):
+        self.start_line = start_line
         self.header = header
         self.body = body
 
-def parse_raw_sip_message(message: str) -> SIPMessage:
-    message = message.splitlines()
-    start_line = message[0]
+def parse_raw_sip_message(raw_message: str) -> SIPMessage:
+    message = raw_message.split("\n")
+    start_line = ""
     headers = {}
-    for lines in message[1:]:
-        if "\n\n" in lines:
+    body = ""
+    for index, line in enumerate(message):
+        if index == 0:
+            start_line = line
+            continue
+
+        if line == '' :   
+            body = str(message[index + 1:])
             break
-        else:
-            value = lines.split(":", 1)
-            headers[value[0]] = value[1]
-    body = []
-    print(start_line)
-    print(headers)
-    return None
-    # return SIPMessage(header, body)
+        else :
+            value = line.split(":", 1)
+            headers[value[0]] = value[1].strip()
+            
+    
+    return SIPMessage(start_line, headers, body)
 
 
 SIP_MESSAGE = """INVITE sip:bob@biloxi.com SIP/2.0
@@ -33,6 +38,14 @@ Call-ID: a84b4c76e66710@://atlanta.com
 CSeq: 314159 INVITE
 Contact: <sip:alice@://atlanta.com>
 Content-Type: application/sdp
-Content-Length: 142"""
+Content-Length: 142
+
+v=0
+o=alice 2890844526 2890844526 IN IP4 ://atlanta.com
+s=-
+c=IN IP4 ://atlanta.com
+t=0 0
+m=audio 49170 RTP/AVP 0
+a=rtpmap:0 PCMU/8000"""
 parse_raw_sip_message(SIP_MESSAGE)
 

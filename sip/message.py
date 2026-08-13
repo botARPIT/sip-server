fdb9f21b3559ee6170d496fc026900ccb44d5784
Arpit Chauhan <arpit@Arpits-MacBook-Air.local>
2026-08-13 10:17:42 +0530

    refactor: change parsing logic
    feat: add serialization

--- sip/message.py
+++ sip/message.py
@@ -3,31 +3,54 @@
 
 class SIPMessage:
 
-    def __init__(self, start_line, header, body):
+    def __init__(self, start_line, headers, body):
         self.start_line = start_line
-        self.header = header
+        self.headers = headers
         self.body = body
 
+
+    def __repr__(self):
+        return (
+            f"SIPMessage("
+            f"start_line={self.start_line!r}"
+            f"headers={self.headers!r}"
+            f"body={self.body!r})"
+        )
+
+    @property
+    def method(self):
+        return self.start_line.split()[0]
+
+    @property
+    def request_uri(self):
+        return self.start_line.split()[1]
+
+    @property
+    def version(self):
+        return self.start_line.split()[2]
+
 def parse_raw_sip_message(raw_message: str) -> SIPMessage:
-    message = raw_message.split("\n")
-    start_line = ""
-    headers = {}
-    body = ""
-    for index, line in enumerate(message):
-        if index == 0:
-            start_line = line
-            continue
+    header_section, body = raw_message.split("\n\n", 1)
+    start_line, headers = header_section.split("\n", 1)
+    headers = headers.split("\n")
+    parsed_headers = {}
 
-        if line == '' :   
-            body = str(message[index + 1:])
-            break
-        else :
-            value = line.split(":", 1)
-            headers[value[0]] = value[1].strip()
-            
-    
-    return SIPMessage(start_line, headers, body)
+    for line in headers:
+        name, value = line.split(":", 1)
+        parsed_headers[name] = value.strip()
 
+    return SIPMessage(start_line, parsed_headers, body)
+
+
+def serialize_sip_message(message: SIPMessage) -> str:
+    lines = []
+    lines.append(message.start_line)
+    for name, value in message.headers.items():
+        lines.append(f"{name}: {value}")
+    lines.append("")
+    lines.append(message.body)
+    print(lines)
+    return "\r\n".join(lines)
 
 SIP_MESSAGE = """INVITE sip:bob@biloxi.com SIP/2.0
 Via: SIP/2.0/UDP ://atlanta.com;branch=z9hG4bK776asdhds
@@ -47,5 +70,7 @@ c=IN IP4 ://atlanta.com
 t=0 0
 m=audio 49170 RTP/AVP 0
 a=rtpmap:0 PCMU/8000"""
-parse_raw_sip_message(SIP_MESSAGE)
+message = parse_raw_sip_message(SIP_MESSAGE)
+print(serialize_sip_message(message))
+
 
